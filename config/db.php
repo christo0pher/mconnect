655cfd7d841d3ca739a02d3fe5b78202932e255d
Christopher Wulff <christopher.wulff@bhmgrp.com>
2019-07-09 10:11:54 +0200

    changed db password

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=musconnect',
     'username' => 'musconnect',
-    'password' => 'a%4qtI29',
+    'password' => 'asdasdasdasd123123asdasdasd!2312312',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
